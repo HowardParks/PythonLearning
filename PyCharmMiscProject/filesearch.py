b105ebcd0c7dc5709f1277e8c7ef83287d903e40
Howard Parks <hparks@werner.com>
2025-04-30 15:26:40 -0500

    Update to current state

---
+++ PyCharmMiscProject/filesearch.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+def search_pathlib(path, target):
+    path = Path(path)
+    lsst = ''
+
+    for item in path.rglob(target):
+        lsst += str(item) + "\n"
+    return lsst
+# def list_contents(path):
+#     path = Path(path)
+#     for item in path.iterdir():
+#         print(f"{'File ' if item.is_file() else 'Dir '}{str(item)}")
+#current_dir = Path.cwd()
+current_dir = input("Search directory: ")
+while current_dir != 'END':
+    if current_dir == '':
+        current_dir = 'C://Users/hparks/OneDrive - Werner Enterprises'
+    elif current_dir == 'Git':
+        current_dir = 'C://Users/hparks/GitHub/WernerEnterprise'
+    elif current_dir == '210':
+        current_dir = 'C://Users/hparks/GitHub/WernerEnterprise/Mastery/210'
+    target = input("Search target: ")
+    found_list = search_pathlib(current_dir, target)
+    print(found_list)
+    current_dir = input("Search Directory: ")
