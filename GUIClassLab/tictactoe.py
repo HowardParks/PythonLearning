4651a566f4331ef8e6c349b9a33f759763208f28
HowardParks <gottobtru@gmail.com>
2025-06-19 15:27:35 -0500

    Copy before refactor

--- GUIClassLab/tictactoe.py
+++ GUIClassLab/tictactoe.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 import tkinter.messagebox as messagebox
+import random
 
 class TicTac(tk.Canvas):
     def __init__(self, parent, index, **kwargs):
         super().__init__(parent, **kwargs)
         self.index = index
+        self.square = ''
 
 def drawx(c):
     c.create_line(7,7,29,29,fill='Red',width=3)
@@ -24,31 +26,55 @@ def solved(player, sofar):
 
 def clicked(event):
     global player, window, xes, oes
-    if event.widget.index in xes | oes:
+    try:
+        print(f"Canvas {event.widget.index} clicked")
+        if event.widget.index in xes | oes:
+            return
+    except AttributeError:
         return
     if player == 0:
         drawx(event.widget)
         xes.add(event.widget.index)
+        event.widget.square = "X"
         solved(player,xes)
     else:
         drawo(event.widget)
         oes.add(event.widget.index)
+        event.widget.square = "O"
         solved(player,oes)
     player = 1 - player
 
+def computermove():
+    while True:
+        choice = random.randint(0,8)
+        if canvases[choice].square == '':
+            break
+    canvases[choice].event_generate('<1>', x=10, y=10)
+
+def playgame():
+    global xes, oes, player
+    print('Game started')
+    while len(xes) + len(oes) < 9:
+        computermove()
+        messagebox.showinfo(message="Your turn!")
+    messagebox.showinfo(title="GameOver", message="Cat's Game!")
+    window.destroy()
+
 window=tk.Tk()
 canvases = []
-bindings = []
 for row in range(3):
     for col in range(3):
         index = row*3 + col
         canvases.append(TicTac(window, index, bg="DarkGray",bd=2,height=30,width=30))
         canvases[index].grid(row=row, column=col)
-        bindings.append(canvases[index].bind('<1>', func=clicked))
+        canvases[index].bind('<1>', func=clicked)
 #window.bind_all('<1>', func=clicked)
+starter = tk.Button(text="Start", command=playgame)
+starter.grid(row=3,column=1)
 
 xes = set(())
 oes = set(())
 player = 0
 
 window.mainloop()
+
