a037469306dead7c811760cf94d399a6ac24e51e
HowardParks <gottobtru@gmail.com>
2025-06-26 12:11:01 -0500

    One lab finished, starting another

--- RESTClassLab/servercheckreq.py
+++ RESTClassLab/servercheckreq.py
@@ -6,7 +6,7 @@ if len(sys.argv) not in [2, 3]:
     sys.exit(1)
 
 # Write your code here.
-server_addr = sys.argv[1]
+server_addr = f"http://{sys.argv[1]}"
 if len(sys.argv) == 2:
     port = 80
 else:
@@ -15,23 +15,13 @@ if not 0 < port <= 65535:
     print("Invalid port number")
     sys.exit(2)
 try:
-    url =  f"{server_addr}:{port/}"
-    request = requests.head(url)
-    re
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.connect((server_addr, port))
-    # sock.send(b"HEAD / HTTP/1.1\r\nHost: " +
-    #           bytes(server_addr, "utf8") +
-    #           b"\r\nConnection: close\r\n\r\n")
-    # reply = sock.recv(10000)
-    # sock.shutdown(socket.SHUT_RDWR)
-    # sock.close()
-    replystr = reply.decode()
-    print(replystr[:replystr.index('\r')])
-except socket.timeout:
-    print("Connection timed out")
+    url =  f"{server_addr}:{port}"
+    reply  = requests.head(url)
+    print(reply.status_code)
+except requests.exceptions.ReadTimeout as e:
+    print(e)
     sys.exit(3)
-except:
-    print("Unknown mode of failure")
+except Exception as e:
+    print(e)
     sys.exit(4)
 sys.exit(0)
